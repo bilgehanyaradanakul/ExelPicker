e482b671c0fb114d97e91b27c8bf613978d1725e
Bilgehan Yaradanakul <bilgehan.yaradanakul60@gmail.com>
2024-05-13 11:42:23 +0300

    App js dosyasi degisti

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Artik bunu degistirdim</Text>
       <StatusBar style="auto" />
     </View>
   );
